50f2ece66e3dbbb7dfdfb00b338c9af0e98a7c93
Prates <guipratesdsantos@gmail.com>
2023-07-21 15:05:10 -0300

    projeto01 refatorado

--- src/js/index.js
+++ src/js/index.js
@@ -2,65 +2,55 @@ const btnAvancar = document.getElementById('btn-avancar');
 const btnVoltar = document.getElementById('btn-voltar');
 const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
-const somFundo = document.getElementById('som');
 
-    function eventoCartaoSelecionado() {
-        const cartaoSelecionado = document.querySelector('.selecionado');
-        cartaoSelecionado.classList.remove('selecionado');
-    };
+function eventoCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+};
 
-    function mostrarCartao(indiceCartao) {
-        cartoes[indiceCartao].classList.add('selecionado');
-    };
+function mostrarCartao(indiceCartao) {
+    cartoes[indiceCartao].classList.add('selecionado');
+};
 
-    btnAvancar.addEventListener('click', function () {
-        if(cartaoAtual === cartoes.length - 1) return;
-        
-        eventoCartaoSelecionado();
-        
-        cartaoAtual++;
-        mostrarCartao(cartaoAtual);
-        
-        if(cartaoAtual === 0) {
-            const audio1 = document.getElementById('audio-1').play();
-        }
+btnAvancar.addEventListener('click', function () {
+    if (cartaoAtual === cartoes.length - 1) return;
 
-        if(cartaoAtual === 1) {
-            const audio2 = document.getElementById('audio-2').play();
-        }
+    eventoCartaoSelecionado();
 
-        if(cartaoAtual === 2) {
-            const audio3 = document.getElementById('audio-3').play();
-        }
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
 
-    });
+    if (cartaoAtual === 0) {
+        const audio1 = document.getElementById('audio-1').play();
+    } else if (cartaoAtual === 1) {
+        const audio2 = document.getElementById('audio-2').play();
+    } else if (cartaoAtual === 2) {
+        const audio3 = document.getElementById('audio-3').play();
+    }
+});
 
-    btnVoltar.addEventListener('click', function () {
-        if(cartaoAtual === 0) return;
+btnVoltar.addEventListener('click', function () {
+    if (cartaoAtual === 0) return;
 
-        eventoCartaoSelecionado();
-        
-        cartaoAtual--;
-        mostrarCartao(cartaoAtual);
-    
-        if(cartaoAtual === 0) {
-            const audio1 = document.getElementById('audio-1').play();
-        }
+    eventoCartaoSelecionado();
 
-        if(cartaoAtual === 1) {
-            const audio2 = document.getElementById('audio-2').play();
-        }
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
 
-        if(cartaoAtual === 2) {
-            const audio3 = document.getElementById('audio-3').play();
-        }
+    if (cartaoAtual === 0) {
+        const audio1 = document.getElementById('audio-1').play();
+    }
 
-    });
+    if (cartaoAtual === 1) {
+        const audio2 = document.getElementById('audio-2').play();
+    }
 
-    // somFundo.addEventListener('click', function () {
-    //     var musica = document.getElementById('musica');
-        
-    //     musica.volume = 0.1;
-        
-    //     musica.play();
-    // });
+    if (cartaoAtual === 2) {
+        const audio3 = document.getElementById('audio-3').play();
+    }
+
+});
+
+const somFundo = document.getElementById('musica');
+somFundo.volume = 0.1;
+somFundo.play();
